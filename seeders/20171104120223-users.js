368516656934f407cca16660595f59f067f93a43
“Dandan <dandanlin.l@gmail.com>
2017-11-04 10:27:14 -0400

    add seeders

--- seeders/20171104120223-users.js
+++ seeders/20171104120223-users.js
@@ -1,33 +1,32 @@
 'use strict';
-
 module.exports = {
    up: (queryInterface, Sequelize) => {
       // Return a promise to correctly handle asynchronicity.
       return queryInterface.bulkInsert('Users', [{
          username: 'user1',
          email: 'user1@test.com',
-         password_hash: 'user1pass',
+         password_hash: 'testpass',
          balance: 1,
          createdAt: new Date(),
          updatedAt: new Date()
       }, {
          username: 'user2',
          email: 'user2@test.com',
-         password_hash: 'user2pass',
+         password_hash: 'testpass',
          balance: 2,
          createdAt: new Date(),
          updatedAt: new Date(),
       }, {
          username: 'user3',
          email: 'user3@test.com',
-         password_hash: 'user3pass',
+         password_hash: 'testpass',
          balance: 3,
          createdAt: new Date(),
          updatedAt: new Date(),
       }, {
          username: 'user4',
          email: 'user4@test.com',
-         password_hash: 'user4pass',
+         password_hash: 'testpass',
          balance: 4,
          createdAt: new Date(),
          updatedAt: new Date(),
