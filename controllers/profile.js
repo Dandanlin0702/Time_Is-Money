7a2338bd92dd8cfe2382256ebbe9fad7d95967c4
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-11-08 20:39:40 -0500

    add offer form

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const User = require('../models/user');
+const models = require('../models');
 const passport = require('../middlewares/authentication');
 const redirect = require('../middlewares/redirect');
 
@@ -8,12 +8,29 @@ const Controller = {
     const router = express.Router();
 
     router.get('/', redirect.isLoggedIn, this.index);
+    router.get('/new', redirect.isLoggedIn, this.new);
+    router.post('/', redirect.isLoggedIn, this.create);
+    router.get('/:id', redirect.isLoggedIn, this.show);
 
     return router;
   },
   index(req, res) {
     res.render('profile', { user: req.user, success: req.flash('success')});
+  },
+  new(req, res) {
+    models.SubCategory.findAll()
+    .then((subcategories) => {
+      res.render('profile/offer_form', {user: req.user, subcategories: subcategories});
+    })
+  },
+  create(req, res) {
+    res.redirect('profile/offer_form_show');
+  },
+  show(req, res) {
+    res.render('profile/offer_form_show');
   }
+
+
 };
 
 module.exports = Controller.registerRouter();
