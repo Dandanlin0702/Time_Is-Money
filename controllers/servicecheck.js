7ab3505bc022c363dbacb9ec075e2be6440cdf95
“Dandan <dandanlin.l@gmail.com>
2017-11-12 11:46:58 -0500

    add servicecheck

--- controllers/servicecheck.js
+++ controllers/servicecheck.js
@@ -1,18 +1,40 @@
 const express = require('express');
-const models = require('../models/');
-const passport = require('../middlewares/authentication');
+// const passport = require('../middlewares/authentication');
+// const redirect = require('../middlewares/redirect');
 
 const Controller = {
   registerRouter() {
     const router = express.Router();
 
     router.get('/', this.index);
+	router.get('/', this.show);
 
     return router;
   },
   index(req, res) {
-    res.render('servicecheck');
-  }
+  	res.render('servicecheck')
+  },
+  show(req, res) {
+  	models.Service.findOne({
+  		where:{
+  			username: req.params.username,
+  		}
+  	}).then((user) => {
+  		models.Service.findOne({
+  			where: {
+  				UserId: user.id,
+  			}
+  		}).then((service) =>{
+  			models.RequestedService.findOne({
+  				where: {
+  					ServiceId: service.id,
+  				}
+  			}).then((requested_service) => {
+  				res.render(Service.service_name)
+  			});
+  		});
+  	});
+  },
 };
 
 module.exports = Controller.registerRouter();
