8b7c43a804bcb73d1c7e861bf46afcd219e017a2
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-11-18 16:36:10 -0500

    grab data in controller and store in model

--- controllers/profile.js
+++ controllers/profile.js
@@ -10,7 +10,9 @@ const Controller = {
     router.get('/', redirect.isLoggedIn, this.index);
     router.get('/new', this.new);
     router.post('/', this.create);
+    router.get('/get_subcategory', this.get_subcategory);
     router.get('/:id', this.show);
+
 
     return router;
   },
@@ -18,18 +20,43 @@ const Controller = {
     res.render('profile', { user: req.user, success: req.flash('success')});
   },
   new(req, res) {
-    res.render('profile/offer_form', {layouts: true});
-    // models.SubCategory.findAll()
-    // .then((subcategories) => {
-    //   res.render('profile', { user: req.user, subcategories: subcategories});
-    // });
+    models.Category.findAll()
+    .then((categories) => {
+      models.SubCategory.findAll()
+      .then((subcategories) => {
+        res.render('profile/offer_form', { user: req.user, categories: categories, subcategories: null});
+      })
+    });
   },
   create(req, res) {
-    res.redirect('profile/offer_form_show');
+    models.Service.create({
+      service_name: req.body.title,
+      description: req.body.description,
+      location: req.body.location,
+      UserId: req.user.id,
+      SubCategoryId: req.body.subcategory
+    }).then(() => {
+      res.redirect('/');
+    })
+    //res.redirect('profile/offer_form_show');
+  },
+  get_subcategory(req, res) {
+    //console.log(req.query.category_id);  //memo: query come from get  after ?
+    models.SubCategory.findAll({
+      where: {
+        CategoryId: req.query.category_id
+      }
+    })
+    .then((subcategories) => {
+      res.render('profile/subcategory_select', {layout: false, subcategories: subcategories});
+      //res.render('profile/subcategory_select', {layout: false});
+    });
+    // res.render('profile/subcategory_select', {layout: false});
   },
   show(req, res) {
     res.render('profile/offer_form_show');
   },
+
 
 
 };
