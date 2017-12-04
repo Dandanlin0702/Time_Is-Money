d32392a816d2a702bb304c06d8159b75f2417673
“Dandan <dandanlin.l@gmail.com>
2017-12-04 16:31:35 -0500

    Activity Page Update

--- controllers/activity.js
+++ controllers/activity.js
@@ -4,57 +4,57 @@ const passport = require('../middlewares/authentication');
 const redirect = require('../middlewares/redirect');
 
 const Controller = {
-  registerRouter() {
-    const router = express.Router();
+   registerRouter() {
+      const router = express.Router();
 
-    router.get('/', redirect.isLoggedIn, this.index);
-    router.get('/:id', this.show);
-    router.delete('/:id', this.delete);
+      router.get('/', redirect.isLoggedIn, this.index);
+      router.get('/:id', this.show);
+      router.delete('/:id', this.delete);
 
-    return router;
-  },
-  index(req, res) {
-    //offer service
-    models.Service.findAll({
-      where: {
-        UserId: req.user.id
-      }
-    }).then((offered_service) => {
-      models.RequestedService.findAll({
-        include: [
-          {
-            model: models.Service
-          }
-        ],
-        where: {
-          UserId: req.user.id
-        }
-      }).then((services) => {
-        res.render('activity', {
-          servicesreq: services,
-          servicesoff: offered_service
-        });
+      return router;
+   },
+   index(req, res) {
+      //offer service
+      models.Service.findAll({
+         where: {
+            UserId: req.user.id
+         }
+      }).then((offered_service) => {
+         models.RequestedService.findAll({
+            include: [{
+               model: models.Service
+            }],
+            where: {
+               UserId: req.user.id
+            }
+         }).then((services) => {
+            res.render('activity', {
+               servicesreq: services,
+               servicesoff: offered_service
+            });
+         });
       });
-    });
-  },
-  show(req, res) {
-    res.render('profile/offer_form_show');
-  },
-  delete(req, res) {
-    models.RequestedService.destroy({
-      where: {
-        ServiceId: req.params.id
-      }
-    }).then(() => {
-      models.Service.destroy({
-        where: {
-          id: req.params.id
-        }
+   },
+   show(req, res) {
+      res.render('profile/offer_form_show');
+   },
+   delete(req, res) {
+      models.RequestedService.destroy({
+         where: {
+            UserId: req.user.id,
+            ServiceId: req.params.id
+         }
       }).then(() => {
-        res.redirect('/activity');
+         models.Service.destroy({
+            where: {
+               UserId: req.user.id,
+               id: req.params.id
+            }
+         }).then(() => {
+            res.redirect('/activity');
+         });
       });
-    });
-  }
+   }
 };
 
 module.exports = Controller.registerRouter();
