ec1c5b7d140d848f485acd9da095abe19ba725d9
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-12-13 17:09:42 -0500

    done

--- controllers/activity.js
+++ controllers/activity.js
@@ -8,10 +8,10 @@ const Controller = {
       const router = express.Router();
 
       router.get('/', redirect.isLoggedIn, this.index);
-      router.get('/:id', this.show);
+      //router.get('/:id', this.show);
       router.delete('/:id', this.delete);
-      router.get('/accept/:id', this.accept);
-      router.get('/reject/:id', this.reject);
+      router.put('/accept/:id', this.accept);
+      router.put('/reject/:id', this.reject);
       // router.get('/accept/update/:num_hours', this.add_hours);
 
       return router;
@@ -32,6 +32,9 @@ const Controller = {
            }
         }).then((my_requests) => {
           models.RequestedService.findAll({
+            where: {
+              status: "PENDING"
+            },
             include: [{
                model: models.Service,
                where: {
@@ -52,9 +55,9 @@ const Controller = {
         });
      });
    },
-   show(req, res) {
-      res.render('profile/offer_form_show');
-   },
+   // show(req, res) {
+   //    res.render('profile/offer_form_show');
+   // },
    delete(req, res) {
       models.RequestedService.destroy({
          where: {
@@ -73,60 +76,136 @@ const Controller = {
       });
    },
    accept(req, res) {
-      models.RequestedService.update({
-         status: "IN PROGRESS"
-      }, {
-         where: {
-            UserId: req.params.id
-         }
-      }).then((reveived_services) => {
-         models.RequestedService.findAll({
-            include: [{
-               model: models.User,
-               where: {
-                  id: req.params.id
-               },
-               include: [{
-                  model: models.Service,
-                  where: {
-                     UserId: req.params.id
-                  },
-               }]
-            }],
-            where: {
-               UserId: req.params.id
-            },
-         }).then(() => {
-            models.User.increment({
-               balance: reveived_services[0],
-            }, {
-               where: {
-                  id: req.user.id
-               }
-            }).then(() => {
-               models.User.update({
-                  balance: req.user.balance-reveived_services[0]
-               }, {
-                  where: {
-                     id: req.params.id
-                  }
-               }).then(() => {
-                  res.redirect('/activity');
-               });
-            });
-         });
-      });
+
+     models.RequestedService.findAll({
+       include: [{
+          model: models.Service,
+          where: {
+            UserId: req.user.id
+          },
+          include: [{
+            model: models.User
+          }]
+       }],
+       where: {
+          id: parseInt(req.params.id)
+       }
+     }).then((requested_service) => {
+       const original_hours = requested_service[0].Service.User.balance;
+       const num_hours = requested_service[0].num_hours;
+       const serviceId = requested_service[0].ServiceId;
+       const offeringUserId = requested_service[0].Service.UserId;
+
+       const requestUserId = requested_service[0].UserId;
+
+       const total_hours = original_hours + num_hours;
+
+       models.RequestedService.update(
+         {status: "IN PROGRESS"},
+         {where: {
+           id: parseInt(req.params.id)
+         }}
+       );
+
+       models.User.update(
+         {balance: total_hours},
+         {where: {
+           id: offeringUserId
+         }}
+       );
+
+       models.User.findAll({
+          where: {
+            id: parseInt(requestUserId)
+          }
+       }).then((requestedUser) => {
+         //res.send(requestedUser);
+         const original_hours_ru = requestedUser[0].balance;
+
+         const total = original_hours_ru - num_hours;
+
+         models.User.update(
+           {balance: total},
+           {where: {
+             id: requestedUser[0].id
+           }}
+         );
+         res.redirect('/activity');
+       });
+     });
+
+     // models.RequestedService.findById(parseInt(req.params.id)
+     // ).then((requested_service) => {
+     //   include: [{
+     //       model: models.Service,
+     //       where: {
+     //         ServiceId: requested_service.ServiceId
+     //       }
+     //    }]
+     //  }).then((service) => {
+     //    res.json(service);
+     //  //});
+     //   // requested_services.update({
+     //   //   status: "IN PROGRESS"
+     //   // });
+     // //
+     // // }).then((service) => {
+     // //   res.send(service);
+     // //   //models.User.findById(service.)
+     // });
+
+      // models.RequestedService.update({
+      //    status: "IN PROGRESS"
+      // }).then((reveived_services) => {
+      //    models.RequestedService.findAll({
+      //       include: [{
+      //          model: models.User,
+      //          where: {
+      //             id: req.params.id
+      //          },
+      //          include: [{
+      //             model: models.Service,
+      //             where: {
+      //                UserId: req.params.id
+      //             },
+      //          }]
+      //       }],
+      //       where: {
+      //          UserId: req.params.id
+      //       },
+      //    }).then(() => {
+      //       models.User.increment({
+      //          balance: reveived_services[0],
+      //       }, {
+      //          where: {
+      //             id: req.user.id
+      //          }
+      //       }).then(() => {
+      //          models.User.update({
+      //             balance: req.user.balance-reveived_services[0]
+      //          }, {
+      //             where: {
+      //                id: req.params.id
+      //             }
+      //          }).then(() => {
+      //             res.redirect('/activity');
+      //          });
+      //       });
+      //    });
+      // });
    },
    reject(req, res) {
-      models.RequestedService.update({
-         status: "REJECTED"
-      }, {
-         where: {
-            UserId: req.params.id
-         }
-      }).then(() => {
-         res.redirect('/activity');
-      });
+
+
+      // models.RequestedService.update({
+      //    status: "REJECTED"
+      // }, {
+      //    where: {
+      //       UserId: req.params.id
+      //    }
+      // }).then(() => {
+      //    res.redirect('/activity');
+      // });
    },
    // add_hours(req, res) {
    //    models.User.update({
